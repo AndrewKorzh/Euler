0736e77c348095f30f5d3c73a3b5389db2fbbec0
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2023-06-08 02:50:17 +0300

    100

---
+++ Task100/Task100/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task100
+{
+    internal class Program
+    {
+        static double f(long n) {
+            //ЕЕЕЕ математика - ну там равенство с двумя переменными (кол-во синих и всего) - тут - из всего -> в сколько синих
+            return (1 + Math.Sqrt(1 + 2 * n * n - 2*n))/2;
+        }
+        static void Main(string[] args)
+        {
+            long n = 1000000000000;
+           while (true)
+            {
+                n++;
+                if (Convert.ToInt32(f(n)) == Convert.ToDouble(f(n)))
+                {
+                    Console.WriteLine("Вот оно:");
+                    Console.WriteLine($"Всего: {n} -> синих: {f(n)}");
+                    break;
+                }
+                   // Console.WriteLine($"{n} -> {f(n)}");
+            }
+
+        }
+    }
+}
