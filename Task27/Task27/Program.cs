139e24de28ea83e7fa2a0074cd698085aeb430f1
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2023-06-08 01:03:03 +0300

    67 - ухх

---
+++ Task27/Task27/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task27
+{
+    internal class Program
+    {
+        static bool is_prime(int n) {
+            if (n < 0)
+                n = -n;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+
+            }
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            
+            int ko_a = 1000;
+            int ko_b = 1000;
+            int max_n = 0;
+            int max_a = 0; int max_b = 0;
+            for (int a = -ko_a + 1; a < ko_a; a++) { 
+                for (int b = -ko_b; b <= ko_b; b++)
+                {
+                   // Console.WriteLine($"\na: {a}  b :{b}\n");
+                    int n = 0;
+                    while (true) {
+                        if (is_prime(n * n + a * n + b))
+                        {
+                          //  Console.WriteLine($"{n}: {n * n + a * n + b}");
+                            
+                            if (n > max_n)
+                            {
+                                max_n = n;
+                                max_a = a;
+                                max_b = b;
+
+                            }
+                        }
+                        else
+                            break;
+                        n++;
+                    }
+             
+
+                }
+            }
+            Console.WriteLine($"max_a: {max_a}, max_b: {max_b}, max_n: {max_n}");
+            Console.WriteLine($"res: {max_a*max_b}");
+        }
+    }
+}
---
+++ Task67/Task67/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Task67
+{
+    
+    internal class Program
+    {
+        static int Max_int(int a, int b) { 
+            if (a > b)
+                return a;
+            return b; 
+        }
+        static void Main(string[] args)
+        {
+            var tr = File.ReadAllLines("triangle.txt");
+            var just_tr= new List<List<int>>();
+            var triangle_of_max = new List<List<int>>();
+            for (int i = 0; i < tr.Length; i++)
+            {
+                var st = tr[i].Split(' ');
+                just_tr.Add(new List<int>());
+                foreach (string x in st)
+                    just_tr[i].Add(int.Parse(x));
+
+            }
+            for (int i = 0; i < just_tr.Count; i++)
+            {
+                Console.WriteLine($"Layer {i + 1}:");
+                for (int j = 0; j < just_tr[i].Count; j++)
+                    Console.Write($"{just_tr[i][j]} ");
+                Console.WriteLine();
+
+
+            }
+            //Realisation
+            //Добавляем вершину
+            triangle_of_max.Add(new List<int>());
+            triangle_of_max[0].Add(just_tr[0][0]);
+            for (int i = 1; i < just_tr.Count; i++)
+            {
+                //добавляем слой
+                triangle_of_max.Add(new List<int>());
+               // Console.WriteLine("test");
+                //Первый эл-т слоя - это первый предыдущего и первый из тогоже слоя исходного
+                triangle_of_max[i].Add(triangle_of_max[i-1][0] + just_tr[i][0]);
+                for (int j = 1; j < just_tr[i].Count-1; j++)
+                {
+                    //Тут просто - выбираем наибольший из тех, из которых можем прийти
+                    triangle_of_max[i].Add(just_tr[i][j] + Max_int(triangle_of_max[i - 1][j], triangle_of_max[i - 1][j - 1]));
+
+                
+                }
+                //Последний эл-т слоя - это последний предыдущего и последний из тогоже слоя исходного
+                triangle_of_max[i].Add(triangle_of_max[i - 1][triangle_of_max[i-1].Count-1] + just_tr[i][just_tr[i].Count - 1]);
+
+
+            }
+            Console.WriteLine("Наш тр-к:");
+            for (int i = 0; i < triangle_of_max.Count; i++)
+            {
+                Console.WriteLine($"Layer {i+1}:");
+                for (int j = 0; j < triangle_of_max[i].Count; j++)
+                    Console.Write($"{triangle_of_max[i][j]} ");
+                Console.WriteLine();
+
+
+            }
+            int res = int.MinValue;
+            foreach (int i in triangle_of_max[triangle_of_max.Count - 1])
+                if (i > res)
+                    res = i;
+
+            Console.WriteLine($"res: {res}");
+        }
+    }
+}
