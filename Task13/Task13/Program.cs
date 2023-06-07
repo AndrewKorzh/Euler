68191bd328808278166efb23a21e7f5ff262fb43
AndrewKorzh <92707967+AndrewKorzh@users.noreply.github.com>
2023-06-07 22:57:04 +0300

    13 14

---
+++ Task13/Task13/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> res = new List<int>();
+            int[,] s = new int[100, 50];
+            long sum = 0;
+            var f = File.ReadAllLines("numbers.txt");
+            for (int i = 0; i < f.Length; i++)
+            {
+                for (int j = 0; j < f[i].Length; j++)
+                {
+                    s[i,j] = int.Parse(f[i][j].ToString());
+                
+                }
+            }
+
+
+            for (int i = 0; i < s.GetLength(1); i++)
+            {
+                Console.Write($"{i} ");
+                int r_sum = 0;
+                for (int j = 0; j < s.GetLength(0); j++)
+                {
+                    
+                    Console.Write(s[j, i]);
+                    r_sum += s[j, i];
+
+                }
+                
+                res.Insert(0, r_sum);
+                r_sum = 0;
+                Console.WriteLine();
+
+            }
+            List<int> final = new List<int>();
+            Console.WriteLine();
+            for (int i = 0; i < res.Count; i++)
+            {
+                Console.WriteLine($"{i}: {res[i]} ");
+                final.Insert(0, (res[i] % 10));
+
+            }
+
+            for (int i = 0; i < final.Count; i++)
+            {
+                
+                final.Add(res[i] % 10);
+                Console.WriteLine($"{final[i]}");
+            }
+
+            //Потом подумаю  
+            // Console.WriteLine($"Res: {sum}");
+        }
+    }
+}
---
+++ Task14/Task14/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task14
+{
+    internal class Program
+    {
+   
+        static void Main(string[] args)
+        {
+            int max = 0;
+            int n = 1000000;
+           for (int i = 2; i < n; i++)
+           {
+                int cl = chain_len(i);
+                if (cl > max ) max = cl;
+           }
+            Console.WriteLine($"Res: {max}");
+        }
+        static int chain_len(int n)
+        {
+            int count = 1;
+            while (n > 1) {
+                //Console.Write($"{n} -> " );
+                count++;
+                if (n % 2 == 0)
+                {
+                    n /= 2;
+                }
+                else n = n * 3 + 1;
+              
+            }
+           // Console.WriteLine(n);
+
+            return count;
+        }
+    }
+}
